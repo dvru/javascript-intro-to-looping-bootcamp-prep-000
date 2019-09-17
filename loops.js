638685da77621c73c43887eba16c586712ecec53
Damini Varu <damvaru4@gmail.com>
2019-09-17 01:20:49 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,24 +1,30 @@
-function forLoop(array) { 
-for(var i = 0; i < 25; i++) {
-  if (i === 1) {
-     array.push('I am 1 strange loop.');
- } else if (i < 25) {
-    array.push('I am ' + i + ' strange loops.');
-     } 
+function forLoop(array) {
+    for ( let i = 0; i < 25; i++) {
+     if (i === 1) {
+       array.push(`I am 1 strange loop.`)
+      } else {
+        array.push(`I am ${i} strange loops.`)
+      }
    }
    return array;
  }
 
  function whileLoop(n) {
- while(n > 0)
-  {console.log(n--)}
-  return "done";
-}
+   let countdown = n;
 
-function maybeTrue(){
-}
+     while (countdown > 0) {
+       console.log(--countdown)
+    }
+    return "done";
+  }
+
 function doWhileLoop(array) {
-  do { array.shift();
-} while (array.length > 0 && maybeTrue());
-return array;
-} 
+  function maybeTrue() {
+     return Math.random() >= 0.5
+   }
+
+   do {
+     array.pop();
+   } while (array.length > -1 && maybeTrue());
+     return array;
+ }
